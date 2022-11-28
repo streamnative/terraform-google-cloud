db685d7bcc9c15b0e7b4c32d010dc673902a7446
Mao Geng <mao.geng@streamnative.io>
2022-11-28 12:08:48 -0800

    remove unused outputs

--- outputs.tf
+++ outputs.tf
@@ -22,14 +22,4 @@ output "service_account" {
   value = module.gke.service_account
 }
 
-output "cert_manager_sa_email" {
-  value = module.cert_manager_sa.gcp_service_account_email
-}
 
-output "external_dns_manager_sa_email" {
-  value = module.external_dns_sa.gcp_service_account_email
-}
-
-output "external_secrets_sa_email" {
-  value = module.external_secrets_sa.gcp_service_account_email
-}
