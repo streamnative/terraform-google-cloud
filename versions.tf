8dd1139b3e45c974fab1df480aa48d380ca4ae19
Max Xu <xuhuan@live.cn>
2023-04-03 11:27:19 +0800

    chore: set version range rather than exact
    Signed-off-by: Max Xu <xuhuan@live.cn>

--- versions.tf
+++ versions.tf
@@ -23,7 +23,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~>4.0"
+      version = "~> 4.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
@@ -31,11 +31,11 @@ terraform {
     }
     helm = {
       source  = "hashicorp/helm"
-      version = "2.2.0"
+      version = "~> 2.2"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = ">=2.8.0"
+      version = "~> 2.8"
     }
   }
 }
