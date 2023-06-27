bd465c8419961e6113a9735a308421d9e813efe2
Benjamin Nelson <illegalnumbers@users.noreply.github.com>
2023-06-27 10:38:59 -0500

    Add CI build for terraform fmt and validate

--- istio.tf
+++ istio.tf
@@ -17,7 +17,7 @@ locals {
     internet_facing = null,
     internal_only = {
       "networking.gke.io/load-balancer-type" = "Internal"
-      "networking.gke.io/internal-load-balancer-allow-global-access": "true"
+      "networking.gke.io/internal-load-balancer-allow-global-access" : "true"
     }
   }
 }
@@ -46,9 +46,9 @@ module "istio" {
   istio_settings = merge({ "controlPlane.spec.components.cni.namespace" : "istio-system", "controlPlane.spec.values.cni.cniBinDir" : "/home/kubernetes/bin" }, var.istio_settings)
 
   istio_ingress_gateway_service_annotations = lookup(local.lb_annotations, var.istio_network_loadbalancer, local.lb_annotations.internet_facing)
-  kiali_gateway_hosts      = ["kiali.${var.service_domain}"]
-  kiali_gateway_tls_secret = "istio-ingressgateway-tls"
-  kiali_operator_settings  = var.kiali_operator_settings
+  kiali_gateway_hosts                       = ["kiali.${var.service_domain}"]
+  kiali_gateway_tls_secret                  = "istio-ingressgateway-tls"
+  kiali_operator_settings                   = var.kiali_operator_settings
 
   depends_on = [
     helm_release.cert_issuer
