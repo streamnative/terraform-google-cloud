291149b69d75e741e53b7453ebc40a7176d961b3
mitch-hamm <mitch.hamm@streamnative.io>
2024-11-18 14:12:37 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -144,7 +144,7 @@ module "gke" {
   count   = var.enable_private_gke ? 0 : 1
   source  = "terraform-google-modules/kubernetes-engine/google"
   name    = var.cluster_name
-  version = "29.0.0"
+  version = "32.0.0"
 
   add_cluster_firewall_rules        = var.add_cluster_firewall_rules
   add_master_webhook_firewall_rules = var.add_master_webhook_firewall_rules
@@ -186,7 +186,7 @@ module "gke_private" {
   source = "terraform-google-modules/kubernetes-engine/google//modules/private-cluster"
 
   name    = var.cluster_name
-  version = "29.0.0"
+  version = "32.0.0"
 
   add_cluster_firewall_rules        = var.add_cluster_firewall_rules
   add_master_webhook_firewall_rules = var.add_master_webhook_firewall_rules
